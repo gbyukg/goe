7908f16b2f5a7f55ff8fd86065d4179bff4976d1
Lock <gbyukg@163.com>
2013-07-06 01:28:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,14 @@ source vim/install_vim.sh
 
 # 安装ag
 # https://github.com/ggreer/the_silver_searcher
+
+#安装js-beautify
+#http://github.com/einars/js-beautify
+#首先需要安装 python的pip
+#sudo apt-get install python-pip
+#使用pip安装jsbeautifier
+#pip jsbeautifier
+
+# C#,C++,C,Java格式化工具
+# sudo apt-get install astyle
+
